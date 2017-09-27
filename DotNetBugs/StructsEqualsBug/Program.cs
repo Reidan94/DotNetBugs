bdd7d374c79e2b03bc452029f8733d8df7750108
a.reidan <a.reidan@plarium.com>
2017-09-27 12:30:07 +0300

    BUG 1: Comparision behavior in structs long vs byte.

--- DotNetBugs/StructsEqualsBug/Program.cs
+++ DotNetBugs/StructsEqualsBug/Program.cs
@@ -1,9 +1,57 @@
-﻿namespace StructsEqualsBug
+﻿using System;
+
+namespace StructsEqualsBug
 {
   static class Program
   {
+    struct LongStruct
+    {
+      long X;
+
+      public override bool Equals(object obj)
+      {
+        return false;
+      }
+
+      public override int GetHashCode()
+      {
+        return 0;
+      }
+    }
+
+    struct ByteStruct
+    {
+      byte X;
+
+      public override bool Equals(object obj)
+      {
+        return false;
+      }
+
+      public override int GetHashCode()
+      {
+        return 0;
+      }      
+    }
+
+    struct LongStructContainer
+    {
+      LongStruct A;
+    }
+
+    struct ByteStructContainer
+    {
+      ByteStruct A;
+    }
+
     static void Main()
     {
+      var longStruct = new LongStructContainer();
+      var byteStruct = new ByteStructContainer();
+
+      Console.WriteLine(byteStruct.Equals(byteStruct));
+      Console.WriteLine(longStruct.Equals(longStruct));
+      Console.ReadLine();
     }
   }
 }
